7a7ebf4d31473be2354c95889c27aa422abb642a
analyn pureza <analyn.c@hotmail.com>
2022-06-19 23:00:36 -0500

    Updated app.js.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var main = function () {
-    var toDos = ["Get groceries",
-                 "Make up some new ToDos",
-                 "Prep for Monday's class",
-                 "Answer emails",
-                 "Take Gracie to the park",
-                 "Finish writing this book"];
+var main = function (toDoObjects) {
+    "use strict";
+    console.log("SANITY CHECK");
+    var toDos = toDoObjects.map(function (toDo) {
+          // we'll just return the description
+          // of this toDoObject
+          return toDo.description;
+    });
 
     $(".tabs a span").toArray().forEach(function (element) {
         var $element = $(element);
@@ -21,33 +22,89 @@ var main = function () {
             $("main .content").empty();
 
             if ($element.parent().is(":nth-child(1)")) {
-                // newest first, so we have to go through
-                // the array backwards
                 $content = $("<ul>");
                 for (i = toDos.length-1; i >= 0; i--) {
                     $content.append($("<li>").text(toDos[i]));
                 }
             } else if ($element.parent().is(":nth-child(2)")) {
-                // oldest first, so we go through the array forwards
                 $content = $("<ul>");
                 toDos.forEach(function (todo) {
                     $content.append($("<li>").text(todo));
                 });
-            } else if ($element.parent().is(":nth-child(3)")) {
-                // input a new to-do
-                $input = $("<input>"),
-                $button = $("<button>").text("+");
 
-                $button.on("click", function () {
-                    if ($input.val() !== "") {
-                        toDos.push($input.val());
-                        $input.val("");
-                    }
+            } else if ($element.parent().is(":nth-child(3)")) {
+                var tags = [];
+
+                toDoObjects.forEach(function (toDo) {
+                    toDo.tags.forEach(function (tag) {
+                        if (tags.indexOf(tag) === -1) {
+                            tags.push(tag);
+                        }
+                    });
+                });
+                console.log(tags);
+
+                var tagObjects = tags.map(function (tag) {
+                    var toDosWithTag = [];
+
+                    toDoObjects.forEach(function (toDo) {
+                        if (toDo.tags.indexOf(tag) !== -1) {
+                            toDosWithTag.push(toDo.description);
+                        }
+                    });
+
+                    return { "name": tag, "toDos": toDosWithTag };
                 });
 
-                $content = $("<div>").append($input).append($button);
-               /* Alternatively append() allows multiple arguments so the above
-                can be done with $content = $("<div>").append($input, $button); */
+                console.log(tagObjects);
+
+                tagObjects.forEach(function (tag) {
+                    var $tagName = $("<h3>").text(tag.name),
+                        $content = $("<ul>");
+
+
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
+
+            } else if ($element.parent().is(":nth-child(4)")) {
+                var $input = $("<input>").addClass("description"),
+                    $inputLabel = $("<p>").text("Description: "),
+                    $tagInput = $("<input>").addClass("tags"),
+                    $tagLabel = $("<p>").text("Tags: "),
+                    $button = $("<span>").text("+");
+
+                $button.on("click", function () {
+                    var description = $input.val(),
+                        tags = $tagInput.val().split(","),
+                        newToDo = {"description":description, "tags":tags};
+
+                    $.post("todos", newToDo, function (result) {
+                        console.log(result);
+
+                        //toDoObjects.push(newToDo);
+                        toDoObjects = result;
+
+                        // update toDos
+                        toDos = toDoObjects.map(function (toDo) {
+                            return toDo.description;
+                        });
+
+                        $input.val("");
+                        $tagInput.val("");
+                    });
+                });
+
+                $content = $("<div>").append($inputLabel)
+                                     .append($input)
+                                     .append($tagLabel)
+                                     .append($tagInput)
+                                     .append($button);
             }
 
             $("main .content").append($content);
@@ -61,7 +118,6 @@ var main = function () {
 
 $(document).ready(function () {
     $.getJSON("todos.json", function (toDoObjects) {
-        // call main with the to-dos as an argument
         main(toDoObjects);
     });
 });
